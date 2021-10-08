fc41b83138b0946f24a3c7e40e05375f5bb2a938
fikrianjani <fikrianjani123@gmail.com>
2021-10-08 11:27:44 +0700

    Struktur Folder, Routes & View
    Ini adalah latihan dari Struktur Folder, Routes & View video ke 3

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Fikri Anjani",
+        "nickname" => "MeruNaru",
+        "image" => "image.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
