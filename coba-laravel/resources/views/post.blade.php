8df4d06a5f34ef0d8095dc12ec35626f9a71569f
fikrianjani <fikrianjani123@gmail.com>
2021-10-15 13:18:17 +0700

    Post Category & Eloquent Relationship
    Ini adalah latihan dari video ke 8 Post Category & Eloquent Relationship

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,6 +2,9 @@
 
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
+
+<p>By Fikri Anjani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </p>
+
 {{ $post->body}}
 <a href="/blog">Back to Posts</a>
 
