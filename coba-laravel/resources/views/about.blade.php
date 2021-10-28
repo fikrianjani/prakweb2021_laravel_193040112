392441b73a8882e88f4c9f5a080245e9574b6474
fikrianjani <fikrianjani123@gmail.com>
2021-10-29 03:43:19 +0700

    N+1 Problem
    Ini adalah latihan dari video ke 11 laravel N+1 Problem

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman About</h1>
+<h1 class="mb-5">Halaman About</h1>
 <h3>{{ $name }}</h3>
 <p>{{ $nickname }} </p>
 <img src="img/{{ $image }}" alt="{{ $name }}" width="300" height="220" class="img-thumbnail rounded-circle">
