1804b2b43709bb4c78446fa63cbd713a7c553ae5
fikrianjani <fikrianjani123@gmail.com>
2021-11-26 02:59:58 +0700

    User Registration
    Latihan dari video User Registration

--- coba-laravel/app/Http/Controllers/PostController.php
+++ coba-laravel/app/Http/Controllers/PostController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = 'in' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = 'by' . $author->name;
+        }
+
         return view('posts', [
             "title" => "All Post",
             "active" => 'posts',
-            "posts" => Post::latest()->filter(request(['search', 'category']))->get()
+            "posts" => Post::latest()->filter(request(['search', 'category']))->paginate(7)->withQueryString()
         ]);
     }
 
